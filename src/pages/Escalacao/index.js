57e96e7afd84b6858c43111bb117d549bd5abca6
FranklinFarias21 <franklinfarias9982@gmail.com>
2023-07-10 22:50:07 -0500

    Adcionando SelectDropdown de formação

--- src/pages/Escalacao/index.js
+++ src/pages/Escalacao/index.js
@@ -2,35 +2,75 @@ import { Text, View, Image, StyleSheet } from "react-native";
 import Cabecalho from "../../components/Cabecalho";
 import Campinho from "../../../assets/campinho.png"
 import { Ionicons } from "@expo/vector-icons"
+import SelectDropdown from 'react-native-select-dropdown'
 
+const Escalacao = () => {
 
-const Escalacao = () => (
-    <View>
-        <Cabecalho />
+    const formacoes = ['3-4-3', '3-5-2', '4-3-3', '4-4-2', '4-5-1', '5-3-2', '5-4-1']
+
+    return (
         <View>
-            <Text>4 3 3</Text>
-        </View>
-        <View style={styles.containerImage}>
-            <Image source={require('cartola/assets/campinho.png')}/>
-        </View>
-        <View>
-            <Text style={styles.tituloBanco}>BANCO DE RESERVAS</Text>
-            <View style={styles.bancoReserva}>
-                <Ionicons name="add-circle-sharp" size={40} color='#ff7400'/>
-                <Ionicons name="add-circle-sharp" size={40} color='#ff7400'/>
-                <Ionicons name="add-circle-sharp" size={40} color='#ff7400'/>
-                <Ionicons name="add-circle-sharp" size={40} color='#ff7400'/>
-                <Ionicons name="add-circle-sharp" size={40} color='#ff7400'/>
+            <Cabecalho />
+            <View style={styles.select}>
+                <SelectDropdown
+                data={formacoes}
+                defaultValueByIndex={2}
+                renderDropdownIcon={isOpened => {
+                    return <Ionicons name={isOpened ? 'chevron-up' : 'chevron-down'} fontWeight={'bold'} color={'#ff7400'} size={18} />
+                  }}
+                buttonStyle={{
+                    width: 100,
+                    height: 40,
+                    marginHorizontal: 10,
+                    backgroundColor: '#fff',
+                    marginTop: 10,
+                    marginBottom: 10,
+                }}
+                buttonTextStyle={{
+                    color: '#ff7400',
+                    fontWeight: 'bold'
+                }}
+                dropdownStyle={{
+                    backgroundColor: '#fff', 
+                    color: "#ff7400",
+                }}
+                rowStyle={{
+                    width: 100,
+                    height: 40,
+                }}
+                rowTextStyle={{
+                    color: "#ff7400",
+                    fontWeight: 'bold'
+                }}
+                />
+            </View>
+            <View style={styles.containerImage}>
+                <Image source={require('cartola/assets/campinho.png')}/>
+            </View>
+            <View>
+                <Text style={styles.tituloBanco}>BANCO DE RESERVAS</Text>
+                <View style={styles.bancoReserva}>
+                    <Ionicons name="add-circle-sharp" size={40} color='#ff7400'/>
+                    <Ionicons name="add-circle-sharp" size={40} color='#ff7400'/>
+                    <Ionicons name="add-circle-sharp" size={40} color='#ff7400'/>
+                    <Ionicons name="add-circle-sharp" size={40} color='#ff7400'/>
+                    <Ionicons name="add-circle-sharp" size={40} color='#ff7400'/>
+                </View>
             </View>
         </View>
-    </View>
-);
+    )
+};
 
     const styles = StyleSheet.create({
+
+        select:{
+            backgroundColor: '#fff',
+            width: 2,
+        }, 
+
         containerImage:{
             display: 'flex',
             alignItems: 'center', 
-            paddingTop: 40,
         },
 
         tituloBanco:{
